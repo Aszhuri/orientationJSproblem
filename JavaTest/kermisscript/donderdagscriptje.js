179020a3f49c5623a7fd05897e37cd9c9a255d8e
Aszhuri <vanluchenea@hotmail.com>
2024-06-13 15:06:47 +0200

    Add files via upload

--- JavaTest/kermisscript/donderdagscriptje.js
+++ JavaTest/kermisscript/donderdagscriptje.js
@@ -3,23 +3,38 @@ function sayHello() {
 }
 
 function askName() {
-  let firstName = prompt("Enter first name:");
-  let lastName = prompt("Enter last name:");
-  let fullName = firstName + " " + lastName;
-  alert("Hello " + fullName);
+  let firstName = prompt('Enter first name:');
+  let lastName = prompt('Enter last name:');
+  let fullName = firstName + ' ' + lastName;
+  if (fullName == 'Alex v') {
+    alert('hellooooo you😎');
+  } else {
+    alert('Hello ' + fullName);
+  }
 }
+
 
 function calcBmi() {
   let weight = prompt("Enter weight (kg):")
   let height = prompt("Enter weight (m):")
   let bmi = weight / (height * height)
-  alert("Your BMI score is: " + bmi)
+ alert("Your BMI score is: " + bmi)
+ if (bmi = <18.5 )
+  Alert("you are underweight.")
+if (bmi = 18.5 below 24.9)
+  alert("You have a normal weight")
+if (bmi = 25 below 29.9)
+alert("You are overweight")
+if (bmi = 30)
+alert("you are OBESE")
 }
 
 function checkAge() {
-    let age = 21 = prompt("Enter your age:")}
+    let age = 21
+    age = prompt("Enter your age:")
   if (age < 18) {
     alert('Have a glass of milk🍼');
 } else if (age >= 18 && age < 65) {
     alert('Have a beer on me🍻');
+}
 }
